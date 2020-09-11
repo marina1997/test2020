3d344927f3d44ebbdfc5d7937e434433fd4e3912
Marina <shemyakina.marina97@gmail.com>
2020-09-11 21:53:05 +0300

    1

---
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+using System.Array;
+//using System.Quantum.Operations;
+
+
+
+
+namespace ClassLibrary1
+{
+    
+    public class quantum
+    {
+        Complex[] qubitVector;
+
+     /*Комплекснае числа
+     *@param no0 - комплексное число, которое передаем 
+     *@param no1 - комплексное число
+     */
+        public void Qubit(Complex no0, Complex no1) {
+		qubitVector = new Complex[2];
+		qubitVector[0] = no0;
+		qubitVector[1] = no1;
+	}
+        /*
+         * @param qubitVector - массив из двух комплексных чисел
+         * копирование массива
+         */
+        public void Qubit(Complex[] qubitVector) {
+      //   this.qubitVector =Array.copyOf(qubitVector, qubitVector.length);
+         Array.Copy(qubitVector, this.qubitVector, qubitVector.Length);
+	}
+
+        public Complex[] getQubit() //getQubit - name
+        {
+            Complex[] copyOfQubitVector = qubitVector;//объявление массива
+            return copyOfQubitVector;
+        }
+
+        public bool isValid(){
+           double sum=0.0;
+          foreach(Complex c in this.qubitVector) {
+               double mod=ComplexMath.mod(c);
+              sum+=mod*mod;
+            }
+    return (sum==1.0);
+}
+
+    }
+}
